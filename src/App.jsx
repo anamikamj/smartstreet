5835f866dbf93f6f58ca436ce218777af3c3c4fa
Karen Teddy Vadakkan <karenteddy2006@gmail.com>
2026-03-07 02:41:04 +0530

    merged member2 and 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+/*import { BrowserRouter, Routes, Route } from "react-router-dom";*/
+/*import { Routes, Route } from "react-router-dom";
 import DangerMap from "./pages/DangerMap";
 import WatchMe from "./pages/WatchMe";
-import TrackView from "./pages/TrackView";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/SOS";
+import TrackView from "./pages/TrackView";*/
+/*import { Routes, Route } from "react-router-dom";*/
+/*import Home from "./pages/SOS";
 import SOS from "./pages/SOS";
 import SOSEvidence from "./pages/SOSEvidence";
 import ERVideoCall from "./pages/ERVideoCall";
@@ -16,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DangerMap />} />
         <Route path="/watchme" element={<WatchMe />} />
-        <Route path="/track/:journeyId" element={<TrackView />} /> {/* trusted contact link */}
+        <Route path="/track/:journeyId" element={<TrackView />} /> 
         <Route path="/" element={<Home />} />
         <Route path="/sos" element={<SOS />} />
         <Route path="/sos/evidence" element={<SOSEvidence />} />
@@ -24,6 +25,32 @@ function App() {
         <Route path="/report" element={<Report />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+export default App; */
+
+import { Routes, Route } from "react-router-dom";
+import DangerMap from "./pages/DangerMap";
+import WatchMe from "./pages/WatchMe";
+import TrackView from "./pages/TrackView";
+import SOS from "./pages/SOS";
+import SOSEvidence from "./pages/SOSEvidence";
+import ERVideoCall from "./pages/ERVideoCall";
+import Report from "./pages/Report";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<DangerMap />} />
+      <Route path="/watchme" element={<WatchMe />} />
+      <Route path="/track/:journeyId" element={<TrackView />} /> {/* trusted contact link */}
+      <Route path="/sos" element={<SOS />} />
+      <Route path="/sos/evidence" element={<SOSEvidence />} />
+      <Route path="/sos/er" element={<ERVideoCall />} />
+      <Route path="/report" element={<Report />} />
+    </Routes>
   );
 }
 
